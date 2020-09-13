83c92e26db7781d4ee20b82ce88d0c9d201fc211
waha1206 <waha1206@gmail.com>
2020-09-14 01:30:24 +0800

    first commit

---
+++ src/components/Demo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="demo">
+    <h2>{{ web }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "demo",
+  data() {
+    return {
+      web: "myoacg.com"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.demo {
+  h2 {
+    color: blue;
+  }
+}
+</style>
---
+++ src/views/User.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="user">
+    <h1>用戶管理</h1>
+    <table
+      class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>編號</th>
+          <th>姓名</th>
+          <th>電話</th>
+          <th>地址</th>
+          <th>編輯</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Waha</td>
+          <td>0918700586</td>
+          <td>新北市新店區</td>
+          <td>刪除</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Waha</td>
+          <td>0918700586</td>
+          <td>新北市新店區</td>
+          <td>刪除</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Waha</td>
+          <td>0918700586</td>
+          <td>新北市新店區</td>
+          <td>刪除</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Waha</td>
+          <td>0918700586</td>
+          <td>新北市新店區</td>
+          <td>刪除</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style lang="scss">
+.user {
+  width: 500px;
+  position: absolute;
+  left: 50%;
+  margin-left: -250px;
+}
+</style>
