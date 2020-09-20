fab8e945e4c51b0810c4c502bd5eafd537968be4
waha1206 <waha1206@gmail.com>
2020-09-21 01:26:50 +0800

    css-html套件移除

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import "./plugins/element.js";
 Vue.config.productionTip = false;
 
 new Vue({
-    router, // eslint-disable-line
-    store, // eslint-disable-line
-    render: h => h(App) // eslint-disable-line
-}).$mount("#app"); // eslint-disable-line
+  router, // eslint-disable-line
+  store, // eslint-disable-line
+  render: (h) => h(App), // eslint-disable-line
+}).$mount('#app'); // eslint-disable-line
